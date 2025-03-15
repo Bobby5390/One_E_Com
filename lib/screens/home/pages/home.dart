f646de3cfd281a0ed0ba4c1bcc816759b2fc75d5
bobby5390 <salakin15-5390@diu.edu.com>
2025-03-15 11:32:15 +0600

    last

---
+++ lib/screens/home/pages/home.dart
@@ -0,0 +1,18 @@
+import 'package:ecommerce_anything/common/widget/appbar.dart';
+import 'package:ecommerce_anything/utils/constants/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:icons_plus/icons_plus.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(  
+      home: FAppBar(),
+    );
+  }
+  
+  
+} 
